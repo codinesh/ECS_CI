9f075d487ee291f9c4d1ef8c309d297d76efc4bc
Codendlearn <codendlearn@outlook.com>
2019-12-13 22:30:46 +0530

    Initial

---
+++ Deploy.Commands/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Deploy.Commands.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("write")]
+        public IActionResult Write()
+        {
+            return Ok("Successfully written.");
+        }
+    }
+}
---
+++ Deploy.Queries/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Deploy.Commands.Queries
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("read")]
+        public IActionResult Write()
+        {
+            return Ok("Successfully read.");
+        }
+    }
+}
